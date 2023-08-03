4f614e48d10d0114ed562eda32fcbd79e480e379
Kuba <kubol2@gmail.com>
2023-08-03 15:06:46 +0200

    Map test update

--- src/test/java/pl/gruchh/restaurant/Mapper/OrderMapperTest.java
+++ src/test/java/pl/gruchh/restaurant/Mapper/OrderMapperTest.java
@@ -2,21 +2,28 @@ package pl.gruchh.restaurant.Mapper;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import pl.gruchh.restaurant.Controller.Dto.OrderDTO;
 import pl.gruchh.restaurant.Entity.Order;
 import pl.gruchh.restaurant.Entity.Pizza;
+import pl.gruchh.restaurant.Service.PizzaService;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class OrderMapperTest {
+
+    @Mock
+    private PizzaService pizzaService;
 
     @InjectMocks
     private OrderMapper orderMapper;
 
     @Test
-    void testMapToOrderDTOWithPizzas() {
+    void ShouldMapToOrderDTOWithCorrectData() {
 
         // Given
         Set<Pizza> pizzaSet = Set.of(
@@ -38,12 +45,13 @@ class OrderMapperTest {
         assertEquals(order.getUsername(), orderDTO.username());
         assertNotNull(orderDTO.pizzas());
         assertEquals(2, orderDTO.pizzas().size());
+        assertEquals(pizzaSet.stream().map(Pizza::getName).collect(Collectors.toSet()), orderDTO.pizzas());
         assertTrue(orderDTO.pizzas().contains("Hawaii"));
         assertTrue(orderDTO.pizzas().contains("Salami"));
     }
 
     @Test
-    void testMapToOrderDTOWithNullPizzas() {
+    void ShouldMapToOrderDTOWithEmptyPizzas() {
 
         // Given
         Set<Pizza> pizzaSet = Set.of(
@@ -64,6 +72,56 @@ class OrderMapperTest {
         assertEquals(order.getUsername(), orderDTO.username());
         assertNotNull(orderDTO.pizzas());
         assertTrue(orderDTO.pizzas().isEmpty());
+    }
+
+    @Test
+    void ShouldMapToOrderWithCorrectData() {
+
+        // Given
+        OrderDTO orderDTO = OrderDTO.builder()
+                .id(133L)
+                .username("admin")
+                .pizzas((Set.of("Hawaiian", "Salami")))
+                .build();
+
+
+        Pizza hawaiian = Pizza.builder().name("Hawaiian").build();
+        Pizza salami = Pizza.builder().name("Salami").build();
+
+        when(pizzaService.getPizzaByName("Hawaiian")).thenReturn(hawaiian);
+        when(pizzaService.getPizzaByName("Salami")).thenReturn(salami);
+
+        // When
+        Order order = orderMapper.mapToOrder(orderDTO);
+
+        // Then
+        assertNotNull(order);
+        assertEquals(orderDTO.id(), order.getId());
+        assertEquals(orderDTO.username(), order.getUsername());
+        assertNotNull(order.getPizzaSet());
+        assertEquals(2, order.getPizzaSet().size());
+        assertTrue(order.getPizzaSet().contains(hawaiian));
+        assertTrue(order.getPizzaSet().contains(salami));
+    }
+
+    @Test
+    void ShouldMapToOrderWithEmptyPizzas() {
+        // Given
+        OrderDTO orderDTO = OrderDTO.builder()
+                .id(133L)
+                .username("admin")
+                .pizzas((Set.of("Hawaiian", "Salami")))
+                .build();
+
+        // When
+        Order order = orderMapper.mapToOrder(orderDTO);
+
+        // Then
+        assertNotNull(order);
+        assertEquals(orderDTO.id(), order.getId());
+        assertEquals(orderDTO.username(), order.getUsername());
+        assertNotNull(order.getPizzaSet());
+        assertTrue(order.getPizzaSet().isEmpty());
     }
 
 }
